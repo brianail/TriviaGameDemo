b6efe7a63bc44cb8a1a7a18d1f1a0c7914dfeefc
brianail <80833852+brianail@users.noreply.github.com>
2022-01-05 21:00:49 -0300

    Initial Commit

---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Brian Ail on 05/01/22.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
